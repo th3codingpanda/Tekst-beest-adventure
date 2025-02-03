a5567eaf8d0e44ca766a90b69125adf519c85602
Thecodingpanda <th3codingpanda@gmail.com>
2025-02-03 17:20:17 +0100

    puah

---
+++ Tekst_beest_adventure/Tekst_beest_adventure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tekst_beest_adventure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
